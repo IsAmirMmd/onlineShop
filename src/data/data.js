d2c0cfd5111d92e72954f7b6df6bcb27a6ca67e6
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-22 20:34:35 +0330

    adding products from static data

---
+++ src/data/data.js
@@ -0,0 +1,81 @@
+export const products = [
+  {
+    id: 1,
+    name: "Winter body",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "ارسال رایگان" },
+      { support: "اورجینال" },
+    ],
+    price: 120,
+    offPrice: 120,
+    discount: 0,
+    image:
+      "https://s6.uupload.ir/files/daniel-storek-jm-qked1gmi-unsplash_ja2.jpg",
+  },
+  {
+    id: 2,
+    name: "Adidas",
+    description: [{ support: "گارانتی مادام العمر" }, { support: "اورجینال" }],
+    price: 110,
+    offPrice: 100,
+    discount: 8,
+    image:
+      "https://s6.uupload.ir/files/andres-jasso-pqbl_mxmaue-unsplash_gkir.jpg",
+  },
+  {
+    id: 3,
+    name: "Vans",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 99,
+    offPrice: 89,
+    discount: 10,
+    image:
+      "https://s6.uupload.ir/files/maksim-larin-nopsc3nwtzy-unsplash_i0j6.jpg",
+  },
+  {
+    id: 4,
+    name: "White",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 260,
+    offPrice: 220,
+    discount: 15,
+    image: "https://s6.uupload.ir/files/revolt-164_6wvehfi-unsplash_xaog.jpg",
+  },
+  {
+    id: 5,
+    name: "Croopped-shoe",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 150,
+    offPrice: 150,
+    discount: 0,
+    image:
+      "https://s6.uupload.ir/files/maksim-larin-ezdrvza1hzw-unsplash_nm8f.jpg",
+  },
+  {
+    id: 6,
+    name: "Blues",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 220,
+    offPrice: 200,
+    discount: 13,
+    image:
+      "https://s6.uupload.ir/files/luis-felipe-lins-j2-waqdckus-unsplash_ku8a.jpg",
+  },
+];
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
+import { products } from "../data/data";
 import Layout from "../Layout/Layout";
 
 const HomePage = () => {
-  return <Layout>HomePage</Layout>;
+  return (
+    <Layout>
+      <div className="product-container">
+        {products.map((p) => (
+          <div className="product-item" key={p.id}>
+            <img src={p.image} alt="item" style={{width:"250px"}} />
+            <p>{p.name}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default HomePage;
