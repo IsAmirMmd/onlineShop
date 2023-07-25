a57e35f30fabe82e6c0017dcf278763c9a1aa0ce
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-25 18:58:49 +0330

    updating cart page structure

--- src/pages/cartPage.jsx
+++ src/pages/cartPage.jsx
@@ -1,7 +1,29 @@
+import { useCart } from "../Context/CartContext";
 import Layout from "../Layout/Layout";
 
+let cartPageInnerHtml = "";
+
 const CartPage = () => {
-  return <Layout>cart page</Layout>;
+  // items in cart
+  const { cart } = useCart();
+  // cart page visibility
+  function renderCartItem() {
+    if (cart.length === 0)
+      return (
+        <main>
+          <h2>No product In Cart !</h2>
+        </main>
+      );
+    else {
+      return cart.map((item) => (
+        <div key={item.id} className="cart--item">
+          <p>{item.name}</p>
+        </div>
+      ));
+    }
+  }
+  // return
+  return <Layout>{renderCartItem()}</Layout>;
 };
 
 export default CartPage;
