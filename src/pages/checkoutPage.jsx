7328eba4923cc9d5126a7d24ceade0a23ebf9e69
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-28 17:26:32 +0330

    adding style to checkout page

--- src/pages/checkoutPage.jsx
+++ src/pages/checkoutPage.jsx
@@ -1,14 +1,14 @@
 import { useCart } from "../Context/CartContext";
 import Layout from "../Layout/Layout";
 import { useAuth } from "../Provider/Auth/AuthProvider";
-
+import { Link } from "react-router-dom";
 const CheckoutPage = () => {
   const userData = JSON.parse(useAuth());
   const { cart } = useCart();
+  console.log(cart);
   let totalPrice = 0;
   return (
     <Layout>
-      test
       {userData ? (
         <div className="checkout-container">
           <div className="user-data">
@@ -17,17 +17,27 @@ const CheckoutPage = () => {
           </div>
           <div className="checkout-items">
             {cart.map((item) => (
-              <div key={item.id}>
-                {item.name}
-                <p>{item.offPrice}</p>
+              <div key={item.id} className="cart-item">
+                <div className="item-img">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div className="item-details">
+                  <p>name : {item.name}</p>
+                  <p>price after off : {item.offPrice}</p>
+                  <p>amount : {item.quantity}</p>
+                </div>
               </div>
             ))}
           </div>
           <div className="checkout-price">
-            {cart.reduce((acc, curr) => {
-              totalPrice += curr.quantity * curr.offPrice;
-            }, 0)}
-            {totalPrice}
+            {cart.length !== 0 ? (
+              cart.reduce((acc, curr) => {
+                totalPrice += curr.quantity * curr.offPrice;
+              }, 0)
+            ) : (
+              <Link to="/">go shopping</Link>
+            )}
+            {totalPrice !== 0 ? <p>total price to pay : {totalPrice} </p> : ""}
           </div>
         </div>
       ) : (
