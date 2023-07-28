990a5c9ea58f8bb657f8f77f60cbee9ffcd53a00
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-28 15:55:09 +0330

    creating authenticator for app

---
+++ src/Provider/Auth/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import  { useContext, useState , createContext } from "react";
+
+export const AuthProviderContext = createContext();
+export const AuthProviderContextDispatcher = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [state, setState] = useState(false);
+
+  return (
+    <AuthProviderContext.Provider value={state}>
+      <AuthProviderContextDispatcher.Provider value={setState}>
+        {children}
+      </AuthProviderContextDispatcher.Provider>
+    </AuthProviderContext.Provider>
+  );
+};
+
+export default AuthProvider;
+
+export const useAuth = () => useContext(AuthProviderContext);
+export const useAuthAction = () => useContext(AuthProviderContextDispatcher);
