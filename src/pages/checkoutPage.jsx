0013fca98bc97b971f2bebded521d863e4eef747
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-28 17:08:39 +0330

    improving checkout page

--- src/pages/checkoutPage.jsx
+++ src/pages/checkoutPage.jsx
@@ -1,9 +1,38 @@
+import { useCart } from "../Context/CartContext";
 import Layout from "../Layout/Layout";
+import { useAuth } from "../Provider/Auth/AuthProvider";
 
 const CheckoutPage = () => {
+  const userData = JSON.parse(useAuth());
+  const { cart } = useCart();
+  let totalPrice = 0;
   return (
     <Layout>
-      <p>checkout page</p>
+      test
+      {userData ? (
+        <div className="checkout-container">
+          <div className="user-data">
+            <p>{userData.name}</p>
+            <p>{userData.email}</p>
+          </div>
+          <div className="checkout-items">
+            {cart.map((item) => (
+              <div key={item.id}>
+                {item.name}
+                <p>{item.offPrice}</p>
+              </div>
+            ))}
+          </div>
+          <div className="checkout-price">
+            {cart.reduce((acc, curr) => {
+              totalPrice += curr.quantity * curr.offPrice;
+            }, 0)}
+            {totalPrice}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </Layout>
   );
 };
